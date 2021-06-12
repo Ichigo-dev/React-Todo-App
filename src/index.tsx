fec2d994c245ea5cbc22a33dc308631a80a2bd1f
ichigo-dev <ichigo@web-freework.com>
2021-06-13 01:26:23 +0900

    routes.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import App from "./pages/App";
-import Task from "./pages/Task";
+import Router from "./routes"
 
 import './index.css';
 
-const name: string = "ichigo";
-
 ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route path="/task">
-        <Task name={name}></Task>
-      </Route>
-      <Route path="/" component={App}></Route>
-    </Switch>
-  </Router>
+  <Router></Router>
   ,document.getElementById("root")
 );
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,19 @@
-import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import App from "./pages/App";
 import Task from "./pages/Task";
 
+const name: string = "ichigo";
+
 const routes = () => (
- <Switch>
-   <Route exact path="/" component={App}></Route>
-   <Route path="/task">
-     <Task name="Ichigo"></Task>
-   </Route>
-   <Route component={App} />
- </Switch>
+  <Router>
+    <Switch>
+      <Route path="/task">
+        <Task name={name}></Task>
+      </Route>
+      <Route path="/" component={App}></Route>
+    </Switch>
+  </Router>
 );
 
 export default routes;
