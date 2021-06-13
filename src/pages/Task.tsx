feaedb2fcd6fbf15a8b4dd872e868c492be059af
ichigo-dev <ichigo@web-freework.com>
2021-06-13 20:23:58 +0900

    useStateフックを使用したCRUD処理

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -1,15 +1,67 @@
-type Props = {
-    name: string
-  }
+import React, { useState } from "react";
 
-const Task = (props: Props) => {
+type Task = {
+  name: string,
+  description: string
+}
+
+const tasks_data: Task[] = [
+  {
+    name: "タスク1",
+    description: "1つ目です。",
+  },
+  {
+    name: "タスク2",
+    description: "2つ目です。",
+  },
+  ]
+
+
+const TaskPage = () => {
+  const [tasks, setTasks] = useState(tasks_data);
+  const [task, setTask] = useState('')
+
+  const handleNewTask = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTask(e.target.value)
+    }
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    if (task === '') return
+    setTasks([...tasks, { name: task, description: "指定なし" }])
+    setTask("")
+    }
+
+  const handleDeleteTask = (index: number) => {
+      const tmp = [...tasks]
+      tmp.splice(index, 1)
+      setTasks(tmp)
+    }
+
+  const handleUpdateTask = (e: any, index: number) => {
+      var tmp = [...tasks]
+      tmp[index].name = e.target.value
+      setTasks(tmp)
+    }
+
   return (
-    <div>
+      <div>
       <h1>タスク一覧</h1>
-      <p>{props.name}</p>
-      <a href="/">Home</a>
-    </div>
-  )
+      <form onSubmit={ handleSubmit }>
+        Add: <input value={ task } onChange={ (e) => handleNewTask(e)}/>
+      </form>
+      <ul>
+        { tasks.map((task, index) => (
+          <p key={ index }>
+            <input key={ index } value={ task.name } onChange={ (e) => handleUpdateTask(e, index)}/>
+            <button key={ index } onClick={() => handleDeleteTask(index)}>削除</button>
+          </p>
+          ))
+        }
+      </ul>
+      <a href="/" >Home</a>
+      </div>
+      )
 };
 
-export default Task;
+export default TaskPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,13 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import App from "./pages/App";
 import Task from "./pages/Task";
 
-const name: string = "ichigo";
-
 const routes = () => (
   <Router>
     <Switch>
       <Route path="/task">
-        <Task name={name}></Task>
+        <Task></Task>
       </Route>
       <Route path="/" component={App}></Route>
     </Switch>
